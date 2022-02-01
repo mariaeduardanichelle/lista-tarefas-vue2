64772581c3c028d43788e78ccb63856cab34cb00
mariaeduardanichelle <95757504+mariaeduardanichelle@users.noreply.github.com>
2022-01-31 23:39:47 -0300

    Add files via upload

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{ valor }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["valor"],
+};
+</script>
+
+<style scoped>
+header {
+  background-color: grey;
+  text-align-last: center;
+  height: 60px;
+  width: 100%;
+  position: fixed;
+  top: 0px;
+}
+</style>
---
+++ src/components/Rodape.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>Meu rodapé</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+  background-color: grey;
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  bottom: 0px;
+  text-align-last: left;
+}
+</style>
